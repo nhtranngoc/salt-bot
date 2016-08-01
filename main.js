1730900cd9d6e1bb1881805e4b5998c8cd7c8c5e
Nam Tran Ngoc <nhtranngoc@wpi.edu>
2016-07-31 23:19:20 -0400

    Finished sound spam

--- main.js
+++ main.js
@@ -3,45 +3,60 @@
 var fs = require('fs');
 var Discord = require('discord.js');
 var auth = require('./config/auth.json');
-// var config = require('./config/config.json');
-
 var bot = new Discord.Client({ autoReconnect: true });
 
 var users = ["Batnam", "PDuck", "Boba", "Mudda", "RC", "Drac"];
+var soundbites = [];
 
 function playSound(thisbot, channel, soundPath) {
 	thisbot.joinVoiceChannel(channel).then(function(connection, joinError){
-			if(joinError){
-				var joinErrorMessage = 'Error joining voice channel: ';
-				console.log(joinErrorMessage, joinError);
-				bot.sendMessage(message.channel, joinErrorMessage + joinError)
-			} 
-			connection.playFile(soundPath, {volume: 1}, function(error, streamIntent) {
-				streamIntent.on("error", function(error) {
+		if(joinError){
+			var joinErrorMessage = 'Error joining voice channel: ';
+			console.log(joinErrorMessage, joinError);
+			bot.sendMessage(message.channel, joinErrorMessage + joinError)
+		} 
+		connection.playFile(soundPath, {volume: 1}, function(error, streamIntent) {
+			streamIntent.on("error", function(error) {
 					// console.log("error " + error);
 					bot.sendMessage(message.channel, 'Error ' + error);
 				})
-				streamIntent.on("end", function() {
+			streamIntent.on("end", function() {
 					// console.log("end");
 					connection.destroy();
 				})
-			})
 		})
+	})
 }
 
+//Check for messages. Be careful not to bog this down
 bot.on('message', function(message) {
-	if (message.content === "What time is it?") {
-		bot.replyTTS(message, "It's high noon");
-	}
-	if (message.content === "Who's the faggot?") {
-		var tempMsg = users[Math.floor(Math.random()*users.length)];
-		bot.reply(message, tempMsg);
-	}
-	if (message.content === "beepboop.mp3") {	
-		playSound(bot, message.author.voiceChannel, "./sounds/beepboop.mp3");
+	if (message.author.username !== bot.user.username) {
+		if (message.content === "What time is it?") {
+			bot.replyTTS(message, "It's high noon");
+		}
+		if (message.content === "Who's the faggot?") {
+			var tempMsg = users[Math.floor(Math.random()*users.length)];
+			bot.reply(message, tempMsg);
+		}
+		if (message.content === "soundls") {
+			bot.sendMessage(message.channel, soundbites);
+		}
+		if (message.content.endsWith(".mp3")){
+			if(soundbites.indexOf(message.content)==-1) {
+				bot.sendMessage(message.channel, "No sound files found. Type soundls for a list of available sound files");
+			} else {
+				playSound(bot, message.author.voiceChannel, "./sounds/" + message.content);
+			}
+		}
 	}
 });
 
 (function init() {
+	//Create a sounddb before initializing
+	fs.readdir('./sounds/', function(err, items){
+		if(err) throw err;
+		soundbites = items;
+	});
 	bot.loginWithToken(auth.token);
+
 })();
